9dacc992e2348a613a6510c858130320ed005da9
Joel Palmius <joepal1976@hotmail.com>
2019-06-28 13:00:02 +0200

    Build numpy array with vertex positions

--- makeclothes/mhmesh.py
+++ makeclothes/mhmesh.py
@@ -1,77 +1,45 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from .mhvgroup import MHVGroup
-from .mhvertex import MHVertex
+import numpy
 
 class MHMesh:
 
-    obj: None
-    vgroups: None
-    vertices: None
-
     def __init__(self, obj):
+
         self.obj = obj
-        self.vgroups = dict()
-        self.vertices = dict()
+        self.vertexGroupVertices = dict()
+        self.vertexGroupNames = dict()
+
+        self._seedGroups = dict()
+
+        if len(obj.vertex_groups) < 1:
+            raise ValueError("This object has no vertex groups. Refusing to continue.")
 
         for group in obj.vertex_groups:
-            if not group.index in self.vgroups:
-                vgroup = MHVGroup(group.name, group.index)
-                self.vgroups[int(group.index)] = vgroup
+            if not group.name in self.vertexGroupNames:
+                self.vertexGroupNames[int(group.index)] = group.name
 
-        for vert in obj.data.vertices:
-            mhvert = MHVertex(vert.index, vert.co[0], vert.co[1], vert.co[2])
-
-            for group in vert.groups:
-                gidx = group.group
-                print(gidx)
-                if not int(gidx) in self.vgroups:
-                    print("Vertex says it has group with index " + str(gidx) + ", but that group does not exist")
+        for vertex in obj.data.vertices:
+            for group in vertex.groups:
+                groupIndex = int(group.group)
+                if not int(groupIndex) in self.vertexGroupNames:
+                    print("Vertex says it has group with index " + str(groupIndex) + ", but that group does not exist. Will ignore this vertex group assignment.")
                 else:
-                    mhvgroup = self.vgroups[int(gidx)]
-                    mhvgroup.addVertex(mhvert)
+                    if not groupIndex in self._seedGroups:
+                        self._seedGroups[groupIndex] = []
+                    seedGroup = self._seedGroups[groupIndex]
 
-            self.vertices[mhvert.index] = mhvert
+                    vertDef = [int(vertex.index), float(vertex.co[0]), float(vertex.co[1]), float(vertex.co[2])] # Index, x, y, z
+                    print("Appending to group " + str(groupIndex) + ": " + str(vertDef))
+                    seedGroup.append(vertDef)
 
-    def findClosestFour(self, foreignVert):
+        for groupIndex in self._seedGroups.keys():
+            seed = self._seedGroups[groupIndex]
+            npa = numpy.zeros((len(seed), 3))
+            for vert in seed:
+                npa[vert[0]][0] = vert[1] # x
+                npa[vert[0]][1] = vert[2] # y
+                npa[vert[0]][2] = vert[3] # z
+            self.vertexGroupVertices[groupIndex] = npa
 
-        if foreignVert is None:
-            raise ValueError("Cannot compare None vertex")
-
-        if len(foreignVert.vgroups) != 1:
-            print("Found foreign vertex with other than exactly one vgroup:")
-            print("  index: " + str(foreignVert.index))
-            print("  groups:")
-            for grp in foreignVert.vgroups:
-                print("    " + grp.name)
-            raise ValueError("Foreign vertex needs exactly one vgroup, has " + str(len(foreignVert.vgroups)))
-
-        groupName = foreignVert.vgroups[0].name
-
-        mhvgroup = None
-
-        for grp in self.vgroups:
-            if grp.name == groupName:
-                mhvgroup = grp
-
-        if mhvgroup is None:
-            raise ValueError("This mesh does not have the " + groupName + " vertex group")
-
-        verts = mhvgroup.vertices
-
-        vert0 = verts[0]
-        firstDistance = vert0.distance(foreignVert)
-        relevantVerts = [ [vert0, firstDistance], [vert0, firstDistance], [vert0, firstDistance], [vert0, firstDistance] ]
-
-        for myVert in verts:
-            distance = myVert.distance(foreignVert)
-            i = 0
-            while i < 4:
-                if distance < relevantVerts[i][1]:
-                    relevantVerts.insert(i, [myVert, distance])
-                    relevantVerts.pop()
-                    i = 4
-                i = i + 1
-
-        return relevantVerts
--- makeclothes/operators/createclothes.py
+++ makeclothes/operators/createclothes.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import bpy, bmesh
+from ..sanitychecks import *
+from ..mhmesh import MHMesh
 
 class MHC_OT_CreateClothesOperator(bpy.types.Operator):
-    """Extract one helper vertex group as clothes"""
+    """Produce MHCLO file and MHMAT, copy textures"""
     bl_idname = "makeclothes.create_clothes"
     bl_label = "Create clothes"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {'REGISTER'}
 
     @classmethod
     def poll(self, context):
@@ -20,5 +22,34 @@ class MHC_OT_CreateClothesOperator(bpy.types.Operator):
         return False
 
     def execute(self, context):
+
+        clothesObj = context.active_object
+
+        if not checkHasAnyVGroups(clothesObj):
+            self.report({'ERROR'}, "This object does not have any vertex group. It has to have at least one for MakeClothes to work.")
+            return {'FINISHED'}
+
+        if not checkAllVerticesBelongToAVGroup(clothesObj):
+            self.report({'ERROR'}, "This object has vertices which do not belong to a vertex group.")
+            return {'FINISHED'}
+
+        if not checkAllVerticesBelongToAtMostOneVGroup(clothesObj):
+            self.report({'ERROR'}, "This object has vertices which belong to multiple vertex groups")
+            return {'FINISHED'}
+
+        if not checkVertexGroupAssignmentsAreNotCorrupt(clothesObj):
+            self.report({'ERROR'}, "This object has vertices which belong non-existing vertex groups, see console for more info")
+            return {'FINISHED'}
+
+        if not checkFacesHaveAtMostFourVertices(clothesObj):
+            self.report({'ERROR'}, "This object has at least one face with more than four vertices. N-gons are not supported by MakeClothes.")
+            return {'FINISHED'}
+
+        if not checkFacesHaveTheSameNumberOfVertices(clothesObj):
+            self.report({'ERROR'}, "This object has faces with different numbers of vertices. Tris *or* quads are supported, but not a mix of the two.")
+            return {'FINISHED'}
+
+        clothesmesh = MHMesh(clothesObj)
+
         self.report({'INFO'}, "Created clothes")
         return {'FINISHED'}
